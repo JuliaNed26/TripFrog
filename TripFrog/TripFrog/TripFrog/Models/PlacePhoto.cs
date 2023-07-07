8cf7b5769b67843a4b4427dece70a925aa0d3a32
JuliaNed26 <nedavnaia@gmail.com>
2023-07-07 11:51:50 +0300

    Deleted unused folders

--- TripFrog/TripFrog/TripFrog/Models/PlacePhoto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TripFrog.Models;
-
-public class PlacePhoto
-{
-    public Guid Id { get; set; }
-    public string PhotoUrl { get; set; }
-    public Guid PlaceId { get; set; }
-    public Place Place { get; set; }
-}
