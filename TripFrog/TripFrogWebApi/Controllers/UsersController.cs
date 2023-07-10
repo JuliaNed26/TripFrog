827f81415f59ca93b27f6d91d9532e444970ab11
JuliaNed26 <nedavnaia@gmail.com>
2023-07-10 14:46:45 +0300

    Added authorization and keyVault

--- TripFrog/TripFrogWebApi/Controllers/UsersController.cs
+++ TripFrog/TripFrogWebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TripFrogWebApi.ActionsClasses;
 using TripFrogWebApi.DTO;
 
@@ -6,46 +7,56 @@ namespace TripFrogWebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class UsersController : ControllerBase
 {
-    private readonly UserService _userActions;
+    private readonly UserService _userService;
 
-    public UsersController(UserService userActions)
+    public UsersController(UserService userService)
     {
-        _userActions = userActions;
+        _userService = userService;
     }
     
     [HttpGet]
     public async Task<ActionResult<IServiceResponse<List<IUserDto>>>> GetUsers()
     {
-        return Ok(await _userActions.GetUsers());
+        return Ok(await _userService.GetUsers());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<IServiceResponse<IUserDto>>> GetUser(Guid id)
     {
-        var response = await _userActions.GetUserById(id);
+        var response = await _userService.GetUserById(id);
         return response.Successful ? Ok(response) : NotFound(response);
     }
 
-    [HttpPost]
-    public async Task<ActionResult<IServiceResponse<IUserDto>>> PostUser(RegisterUserDto registerUser)
+    [HttpPost("register")]
+    [AllowAnonymous]
+    public async Task<ActionResult<IServiceResponse<IUserDto>>> RegisterUser(RegisterUserDto registerUser)
     {
-        var response = await _userActions.RegisterUser(registerUser);
+        var response = await _userService.RegisterUser(registerUser);
+        return response.Successful ? Ok(response) : BadRequest(response);
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult<IServiceResponse<string>>> LoginUser(LoginUserDto loginUser)
+    {
+        var response = await _userService.LoginUser(loginUser);
         return response.Successful ? Ok(response) : BadRequest(response);
     }
 
     [HttpPut]
     public async Task<ActionResult<IServiceResponse<IUserDto>>> ChangeUserInfo(ChangedUserInfoDto user)
     {
-        var response = await _userActions.ChangeUserInfo(user);
+        var response = await _userService.ChangeUserInfo(user);
         return response.Successful ? Ok(response) : NotFound(response);
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult<IServiceResponse<IUserDto>>> DeleteUser(Guid id)
     {
-        var response = await _userActions.DeleteUser(id);
+        var response = await _userService.DeleteUser(id);
         return response.Successful ? Ok(response) : NotFound(response);
     }
 }
---
+++ TripFrog/TripFrogWebApi/JWTTokenCreator.cs
@@ -0,0 +1,53 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TripFrogWebApi.DTO;
+
+namespace TripFrogWebApi;
+
+public sealed class JWTTokenCreator
+{
+    private IConfiguration _configuration;
+
+    public JWTTokenCreator(IConfiguration configuration) => _configuration = configuration;
+    public string CreateJWTToken(IUserDto user)
+    {
+        List<Claim> userClaims = GenerateClaimsList(user);
+        string tokenKey = GetTokenKey();
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var token = new JwtSecurityToken(
+            claims: userClaims, 
+            expires:DateTime.Now.AddMinutes(20),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private List<Claim> GenerateClaimsList(IUserDto user)
+    {
+        return new List<Claim>()
+        {
+            new Claim("Id", user.Id.ToString()),
+            new Claim("FirstName",user.FirstName),
+            new Claim("LastName",user.LastName ?? string.Empty),
+            new Claim("Email",user.Email),
+            new Claim("Phone",user.Phone ?? string.Empty),
+            new Claim("PictureUrl",user.PictureUrl ?? string.Empty),
+            new Claim("Role",user.Role.ToString()),
+        };
+    }
+
+    private string GetTokenKey()
+    {
+        string? tokenKey = _configuration.GetSection("AppSettings:TokenKey").Value;
+        if (tokenKey is null)
+        {
+            throw new ArgumentNullException(nameof(tokenKey));
+        }
+
+        return tokenKey;
+    }
+}
